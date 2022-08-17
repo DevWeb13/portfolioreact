f62385d3adaf902aa3b56b4b31a7df6d7c585666
DevWeb13 <giuliano.loic@sfr.fr>
2022-08-17 19:12:11 +0200

    revert: reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
-import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
@@ -13,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-reportWebVitals();
