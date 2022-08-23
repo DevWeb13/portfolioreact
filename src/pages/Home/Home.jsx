91255ec6d3c0281512c9c5151babea90d9a70a9d
DevWeb13 <giuliano.loic@sfr.fr>
2022-08-23 23:09:14 +0200

    feat: (Home.jsx): Add text + <LogoLinks /> + Responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import ConstructionLogo from '../../components/ConstructionLogo/ConstructionLogo';
+import LogoLinks from '../../components/LogoLinks/LogoLinks';
 
 function Home() {
-  return <ConstructionLogo />;
+  return (
+    <div className="home">
+      <div className="homeContainer">
+        <h1>Developpeur Javascript/React</h1>
+        <p>
+          <span>&quot;La Réponse Dev&quot;</span> vous souhaite la bienvenue.
+        </p>
+        <p>
+          <span>Bonjour à tous !! 👋</span>
+        </p>
+        <p>
+          Je m&apos;appelle <span>Loic</span>.
+          <br />
+          Je suis <span>développeur front-end spécialisé React</span>.
+        </p>
+        <p>
+          Je développe des sites web depuis <span>2019</span> et je suis
+          actuellement la formation
+          <br />
+          <a href="https://openclassrooms.com/fr/paths/516-developpeur-dapplication-javascript-react">
+            &quot; Développeur d&apos;application - JavaScript React&quot;
+          </a>
+          .
+        </p>
+        <p>
+          {' '}
+          En <span>apprentissage permanent</span>, cette formation me permet de
+          <span> renforcer mes acquis</span> et d&apos;
+          <span>acquérir les compétences</span> clés de la profession de
+          développeur.
+        </p>
+
+        <LogoLinks />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
