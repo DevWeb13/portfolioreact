d0083891bffbac21b08ca4d396b2c63dd205d639
DevWeb13 <giuliano.loic@sfr.fr>
2022-08-29 18:12:02 +0200

    fix: (package.json): delete proxy
    (dataManager.js): add entire url

--- src/services/dataManager.js
+++ src/services/dataManager.js
@@ -1,11 +1,14 @@
 export default async function getProjectsList() {
   try {
-    const response = await fetch('/projects', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+    const response = await fetch(
+      'https://backportfoliohero.herokuapp.com/projects',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
       },
-    });
+    );
     return response.json();
   } catch (error) {
     return console.log(error);
