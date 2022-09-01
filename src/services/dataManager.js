5049c29a7cb2fb87badb5111efb6ba24857e0630
DevWeb13 <giuliano.loic@sfr.fr>
2022-09-01 16:42:23 +0200

    feat: (dataManager.js): Replace Heroku by Vercel

--- src/services/dataManager.js
+++ src/services/dataManager.js
@@ -1,7 +1,7 @@
 export default async function getProjectsList() {
   try {
     const response = await fetch(
-      'https://backportfoliohero.herokuapp.com/projects',
+      'https://back-portfolio-devweb13.vercel.app/projects',
       {
         headers: {
           'Content-Type': 'application/json',
