e723d695a617cac21479e62991fa8ce85b60e026
DevWeb13 <giuliano.loic@sfr.fr>
2022-11-21 15:57:51 +0100

    feat: (Projects.jsx): replace state by query
    (index.js): <ReactQueryDevtools initialIsOpen={false} />

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import App from './App';
 
@@ -15,6 +16,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
