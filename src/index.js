5c62e65091e0618c6f134fc3566cb690460e37f5
DevWeb13 <giuliano.loic@sfr.fr>
2022-11-21 14:56:32 +0100

    feat: (index.js): import { QueryClient, QueryClientProvider } from 'react-query';
    const queryClient = new QueryClient(); &&  <QueryClientProvider client={queryClient}>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') || document.body,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
