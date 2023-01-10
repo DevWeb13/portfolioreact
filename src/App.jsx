dce3e59f09ad4089de11407fa067b39d02dff56e
DevWeb13 <giuliano.loic@sfr.fr>
2023-01-10 18:39:20 +0100

    feat: (App.jsx): Loading components not Lazy

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-query';
 import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
 
 const Home = lazy(() => import('./pages/Home/Home'));
 const Projets = lazy(() => import('./pages/Projects/Projects'));
 const Contact = lazy(() => import('./pages/Contact/Contact'));
-const Loading = lazy(() => import('./components/Loading/Loading'));
 
 function App({ isDev }) {
   useQuery('isDev', () => isDev, {
     staleTime: 1000 * 60 * 60 * 24,
     cacheTime: 1000 * 60 * 60 * 24,
   });
-  async function scrollToTop() {
-    await window.scrollTo({
+  useEffect(() => {
+    window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
-  }
-
-  useEffect(() => {
-    scrollToTop();
   }, []);
-
   return (
     <div className="App">
       <BrowserRouter>
